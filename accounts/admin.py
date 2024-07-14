6c3dd116975470e3248e388dde86f2c58a0e3835
nepalsaurav <nepalsaurav123@gmail.com>
2024-07-14 17:27:24 +0545

    v-0.0.1

--- accounts/admin.py
+++ accounts/admin.py
@@ -11,14 +11,7 @@ class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ['email', 'username',]
-    model_fields = [field.name for field in CustomUser._meta.get_fields() if field.concrete]
-    add_form_template = 'admin/user_change_form.html'  # Use the custom template
     change_form_template = 'admin/user_change_form.html'
-    add_fieldsets = (
-    (None, {
-        'classes': ('wide',),
-        'fields': model_fields
-    }),
-)
+   
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/forms.py
+++ accounts/forms.py
@@ -4,33 +4,23 @@ from .models import CustomUser
 from django.contrib.auth.models import Group
 from django.contrib.auth.models import Permission
 
-class CustomUserCreationForm(UserCreationForm):
-    groups = forms.ModelMultipleChoiceField(
+groups = forms.ModelMultipleChoiceField(
         queryset=Group.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         label="User Groups",
+        required=False,
         help_text="Select all groups that apply to this user.",
         error_messages={"required": "Select at least one group."},
     )
-    user_permissions = forms.ModelMultipleChoiceField(
-        queryset=Permission.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        label="User Permissions",
-        help_text="Select all permissions that apply to this user.",
-        error_messages={"required": "Select at least one permission."},
-    )
+
+class CustomUserCreationForm(UserCreationForm):
+    groups = groups
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = ('email',)
 
 class CustomUserChangeForm(UserChangeForm):
-    groups = forms.ModelMultipleChoiceField(
-        queryset=Group.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        label="User Groups",
-        help_text="Select all groups that apply to this user.",
-        error_messages={"required": "Select at least one group."},
-    )
+    groups = groups
     class Meta:
         model = CustomUser
         fields = ('email', 'username',)
